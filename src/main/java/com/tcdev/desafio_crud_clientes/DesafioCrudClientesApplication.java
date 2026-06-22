d7e2ce7e27fb9556d64365777d4389e49e4a492a
tiago-costa-s <tiagocostasantos0013@gmail.com>
2026-06-22 19:50:45 -0300

    first commit

---
+++ src/main/java/com/tcdev/desafio_crud_clientes/DesafioCrudClientesApplication.java
@@ -0,0 +1,13 @@
+package com.tcdev.desafio_crud_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCrudClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCrudClientesApplication.class, args);
+	}
+
+}
