91babc2c769111db1d2d8c9521d67723636b9d88
tiago-costa-s <tiagocostasantos0013@gmail.com>
2026-06-24 21:08:56 -0300

    feat: criação do controlador ClientController

---
+++ src/main/java/com/tcdev/desafio_crud_clientes/controllers/ClientController.java
@@ -0,0 +1,23 @@
+package com.tcdev.desafio_crud_clientes.controllers;
+
+import com.tcdev.desafio_crud_clientes.dto.ClientDTO;
+import com.tcdev.desafio_crud_clientes.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientController {
+    @Autowired
+    private ClientService service;
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO dto = service.findById(id);
+         return ResponseEntity.ok(dto);
+    }
+}
