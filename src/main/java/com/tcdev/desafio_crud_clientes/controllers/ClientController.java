f851b841fa12aae137f2f9e304b413541437a2b2
tiago-costa-s <tiagocostasantos0013@gmail.com>
2026-06-29 20:46:36 -0300

    feat: implementado findAll

--- src/main/java/com/tcdev/desafio_crud_clientes/controllers/ClientController.java
+++ src/main/java/com/tcdev/desafio_crud_clientes/controllers/ClientController.java
@@ -3,11 +3,14 @@ package com.tcdev.desafio_crud_clientes.controllers;
 import com.tcdev.desafio_crud_clientes.dto.ClientDTO;
 import com.tcdev.desafio_crud_clientes.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -18,6 +21,12 @@ public class ClientController {
     @GetMapping(value = "{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         ClientDTO dto = service.findById(id);
-         return ResponseEntity.ok(dto);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
 }
--- src/main/java/com/tcdev/desafio_crud_clientes/services/ClientService.java
+++ src/main/java/com/tcdev/desafio_crud_clientes/services/ClientService.java
@@ -5,8 +5,12 @@ import com.tcdev.desafio_crud_clientes.entities.Client;
 import com.tcdev.desafio_crud_clientes.repositories.ClientRepository;
 import com.tcdev.desafio_crud_clientes.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ClientService {
@@ -19,5 +23,11 @@ public class ClientService {
         Client client = repository.findById(id).orElseThrow(
                 ()-> new ResourceNotFoundException("Recurso não encontrado."));
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> results = repository.findAll(pageable);
+        return results.map((x)-> new ClientDTO(x));
     }
 }
